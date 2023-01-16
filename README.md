38a9f4f6b012b2d43029aa5ac8115f4b38facdcc
Kevin Moonen <kevin.moonen99@gmail.com>
2023-01-16 01:29:39 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# crafting-calculator
+A calculator for what resources are required to build something
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//! The calculator
+
+#![warn(clippy::all)]
+#![warn(missing_copy_implementations, missing_docs, rust_2018_idioms)]
+#![deny(unsafe_op_in_unsafe_fn, missing_debug_implementations)]
+#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
+
+fn main() {
+    println!("Hello, world!");
+}
